6cd6e985a9d9d4c55b65d22747fc3341e36e6726
Kjell Larsson <kjell.m.larsson@se.ibm.com>
2022-12-08 12:29:10 +0100

    initial commit

---
+++ src/main/java/ibmoffices/IBMOffice.java
@@ -0,0 +1,15 @@
+package com.ibm.clientengineering.ibmoffices;
+
+public class IBMOffice {
+
+    public String name;
+    public String description;
+
+    public IBMOffice() {
+    }
+
+    public IBMOffice(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
---
+++ src/main/java/ibmoffices/IBMOfficeResource.java
@@ -0,0 +1,42 @@
+package com.ibm.clientengineering.ibmoffices;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("/offices")
+public class IBMOfficeResource {
+
+    private Set<IBMOffice> offices = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
+
+    public IBMOfficeResource() {
+        offices.add(new IBMOffice("Armonk", "Armonk"));
+        offices.add(new IBMOffice("Astor Place", "Astor Place"));
+        offices.add(new IBMOffice("Austin", "Austin"));
+        offices.add(new IBMOffice("Research Triangle Park", "Research Triangle Park"));
+        offices.add(new IBMOffice("Hursley House", "Hursley House"));
+        offices.add(new IBMOffice("Bangalore", "Bangalore"));
+    }
+
+    @GET
+    public Set<IBMOffice> list() {
+        return offices;
+    }
+
+    @POST
+    public Set<IBMOffice> add(IBMOffice office) {
+        offices.add(office);
+        return offices;
+    }
+
+    @DELETE
+    public Set<IBMOffice> delete(IBMOffice office) {
+        offices.removeIf(existingOffice -> existingOffice.name.contentEquals(office.name));
+        return offices;
+    }
+}
